720d20e056b08a853d6381023fb440bae176010d
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-08-30 17:31:28 +0200

    Create array of array inside of Gameboard()

---
+++ script.js
@@ -0,0 +1,12 @@
+function Gameboard() {
+    const row = 3;
+    const column = 3;
+    const board = [];
+
+    for ( i=0; i<row ; i++ ) {
+        board[i] = [];
+        for ( j=0 ; j<column ; j++) {
+            board[i].push(0);
+        };
+    };
+};
