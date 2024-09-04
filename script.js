94969fbd3619561da86f22066aae0811e5108d00
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-09-04 19:10:36 +0200

    Hardcode board size for efficiency

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function Gameboard() {
-    const row = 3;
-    const column = 3;
     const board = [];
 
-    for ( let i=0; i<row ; i++ ) {
+    for ( let i=0; i<3 ; i++ ) {
         board[i] = [];
-        for ( let j=0 ; j<column ; j++) {
+        for ( let j=0 ; j<3 ; j++) {
             board[i].push(0);
         };
     };
